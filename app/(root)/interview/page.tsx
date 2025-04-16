a6b27485c419a3d8b8c487b39c10efefe2d87fda
kunal-1906 <kunalvig2@gmail.com>
2025-04-16 20:02:19 +0530

    implement interview generation

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,23 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+
+function page() {
+  return (
+   
+    <>
+    <h3>Interview Generation</h3>
+
+    
+
+
+    <Agent userName="You" useId="user1" type="generate"/>
+
+    
+    
+
+    </>
+  )
+}
+
+export default page
