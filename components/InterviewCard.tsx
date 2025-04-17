28f61d1385580a0f7545f7030600facc28ef1707
kunal-1906 <kunalvig2@gmail.com>
2025-04-17 19:29:16 +0530

    show feedback

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -5,9 +5,10 @@ import Image from "next/image";
 import { Button } from "./ui/button";
 import DisplayTechIcons from "./DisplayTechIcons";
 
-import { getRandomInterviewCover } from "@/lib/utils";
+import { cn, getRandomInterviewCover } from "@/lib/utils";
+import { getFeedbackByInterviewId } from "@/lib/actions/general.action";
 
-const InterviewCard = ({
+const InterviewCard = async ({
   id,
   userId,
   role,
@@ -15,30 +16,42 @@ const InterviewCard = ({
   techstack,
   createdAt,
 }: InterviewCardProps) => {
-  const feedback = null as Feedback|null;
+  const feedback =
+    userId && id
+      ? await getFeedbackByInterviewId({
+          interviewId:id,
+          userId,
+        })
+      : null;
+
   const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
+
+  const badgeColor =
+    {
+      Behavioral: "bg-light-400",
+      Mixed: "bg-light-600",
+      Technical: "bg-light-800",
+    }[normalizedType] || "bg-light-600";
+
   const formattedDate = dayjs(
     feedback?.createdAt || createdAt || Date.now()
   ).format("MMM D, YYYY");
 
-        
-
-
   return (
-    
     <div className="card-border w-[360px] max-sm:w-full min-h-96">
       <div className="card-interview">
         <div>
           {/* Type Badge */}
           <div
-            className=
-              "absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600"
-
-            
+            className={cn(
+              "absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg",
+              badgeColor
+            )}
           >
             <p className="badge-text ">{normalizedType}</p>
           </div>
-         
+
+          {/* Cover Image */}
           <Image
             src={getRandomInterviewCover()}
             alt="cover-image"
@@ -62,9 +75,17 @@ const InterviewCard = ({
               <p>{formattedDate}</p>
             </div>
 
-           
+            <div className="flex flex-row gap-2 items-center">
+              <Image src="/star.svg" width={22} height={22} alt="star" />
+              <p>{feedback?.totalScore || "---"}/100</p>
+            </div>
           </div>
-         
+
+          {/* Feedback or Placeholder Text */}
+          <p className="line-clamp-2 mt-5">
+            {feedback?.finalAssessment ||
+              "You haven't taken this interview yet. Take it now to improve your skills."}
+          </p>
         </div>
 
         <div className="flex flex-row justify-between">
